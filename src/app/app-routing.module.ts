7c745d1a36425d715a94b49b140c7955108da123
Манулик Никита Сергеевич <manuliknikita@MacBook-Pro-Manulik.local>
2020-05-04 19:14:45 +0300

    Add hr and correct button(<a><button>Check my resume</button><a/>)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { SkillsComponent } from './components/skills/skills.component';
 
 const routes: Routes = [
+  { path: 'skills', component: SkillsComponent }
 ];
 
 @NgModule({
